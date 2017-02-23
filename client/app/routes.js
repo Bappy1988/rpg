ff0184e8da78d0eaaaeef93cb4861b91965c52ad
Sam Waters <sam@samwaters.com>
2017-02-23 16:40:19 +0000

    Add route with authentication

--- client/app/routes.js
+++ client/app/routes.js
@@ -3,11 +3,19 @@ import {IndexRedirect, Route} from 'react-router';
 import App from './app';
 import HomeComponent from './components/home.component';
 import NotFoundComponent from './components/notfound.component';
+import * as authSelectors from './selectors/auth.selectors';
 
-const getRoutes = (/*store*/) => {
+const getRoutes = (store) => {
+	const requireAuth = (nextState, replace) => {
+		const state = store.getState();
+		if(!authSelectors.selectIsLoggedIn(state)) {
+			replace({pathname: '/home'});
+		}
+	};
 	return <Route path="/" component={App}>
 		<IndexRedirect to="/home" />
 		<Route path="/home" components={{main:HomeComponent}} />
+		<Route path="/secret" components={{main:HomeComponent}} onEnter={(n, r) => requireAuth(n, r)} />
 		<Route path="*" components={{main:NotFoundComponent}} />
 	</Route>
 };
