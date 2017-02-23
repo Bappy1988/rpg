6362f94843a6393c9933df29d79b0ea0b5360939
Sam Waters <sam@samwaters.com>
2017-02-23 16:41:44 +0000

    Add auth reducer

--- client/app/reducers/index.js
+++ client/app/reducers/index.js
@@ -1,6 +1,7 @@
 import {combineReducers} from 'redux';
+import authReducer from './auth.reducer';
 import homeReducer from './home.reducer';
 import {routerReducer} from 'react-router-redux';
 
-const appReducers = combineReducers({home:homeReducer, routing:routerReducer});
+const appReducers = combineReducers({auth:authReducer, home:homeReducer, routing:routerReducer});
 export default appReducers;
