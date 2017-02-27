7cb0dbbc6a4fb5d41c929bc344f1b126d924b531
Sam Waters <sam@samwaters.com>
2017-02-27 17:01:55 +0000

    Update home component

--- client/app/components/home.component.jsx
+++ client/app/components/home.component.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
 import {Button} from 'react-toolbox';
 import {connect} from 'react-redux';
+import {Row, Col} from './common/grid';
+import {Information} from './common/messages.jsx';
 import * as homeActions from '../actions/home.actions';
 
 class Home extends React.Component {
 	render() {
 		return <div>
 			<h3>Home Component</h3>
-			<p>This is the home page</p>
-			<p>Current time: {this.props.time}</p>
-			<Button primary raised onClick={() => this.props.getTime()}>Refresh Time</Button>
+			<Information>This is an information message</Information>
+			<Row>
+				<Col sm={12} md={6}>
+					<p>This is the home page</p>
+					<p>It is using a responsive 12-column grid layout</p>
+				</Col>
+				<Col sm={12} md={6}>
+					<p>Current timestamp: {this.props.time}</p>
+					<Button primary raised onClick={() => this.props.getTime()}>Refresh Time</Button>
+				</Col>
+			</Row>
 		</div>
 	}
 }
