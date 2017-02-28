8c8037f39d1ed262adc67a320681015c3162fcd2
Sam Waters <sam@samwaters.com>
2017-02-28 17:02:29 +0000

    Add grid and messages tests

--- client/app/components/common/grid.jsx
+++ client/app/components/common/grid.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import classNames from 'classnames';
-import * as styles from './grid.styles.scss';
+import * as styles from './grid.styles';
 
 class Row extends React.Component {
 	render() {
