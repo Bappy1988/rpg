4c3471a34813a436e15edec4e5bdc07dfbc7bb07
Bappy1988 <ben@wardmail.org.uk>
2018-04-03 14:45:49 +0100

    fix db route

--- server/api/json.route.js
+++ server/api/json.route.js
@@ -1,4 +1,4 @@
 const server = require('../dev.js');
 const jsonServer = require('json-server');
 
-server.use('/json',jsonServer.router('server/db.json'));
+server.use('/json',jsonServer.router('db/db.json'));
