42cd407197fbe22929c2e16487718200308cb880
Bappy1988 <ben@wardmail.org.uk>
2018-04-13 14:17:13 +0100

    fix nodemon

--- client/app/components/admin/viewscreen.jsx
+++ client/app/components/admin/viewscreen.jsx
@@ -74,77 +74,81 @@ class AdminViewscreen extends React.Component {
             selectedImage = this.getSpecificImage(this.state.selectedImage);
         }
         return <Row>
-            <Col md={6}>
-                <Card raised style={{marginBottom: "16px"}}>
-                    <CardTitle title="Current Display Image"/>
-                    <CardText>
-                        {this.props.currentImage && 
-                        <Row>
-                            <Col md={8}>
-                                {this.props.currentImage.id && <img 
-                                    src={this.props.currentImage.data}
-                                    style={{width: "100%", height: "auto"}}
-                                />}
-                            </Col>
-                            <Col md={4}>
-                                {this.props.currentImage.id && <div><p>
-                                    {this.props.currentImage.name}
-                                </p>
-                                <Button label="unset image" onClick={()=>this.props.setImage(false)}/>
-                                </div>}
-                            </Col>
-                        </Row>}
-                        {!this.props.currentImage && <p>No image has been set.</p>}
-                    </CardText>
-                </Card>
+            <Col md={7}> 
+                <Row>
+                    <Card raised style={{marginBottom: "16px"}}>
+                        <CardTitle title="Upload New Images"/>
+                        <CardText>
+                            <BrowseButton icon="search" name="fileUpload" label="choose file" onChange={(e)=>this.handleFileSelect(e)}/>
+                            {this.state.fileData && <div>
+                                <img style={{width: "100%", height: "auto"}}src={this.state.fileData}/>
+                                <Input type="text" value={this.state.newImageName} onChange={val=>this.setState({newImageName: val})} label="Image name"/>
+                                <Button icon="file_upload" label="upload" raised primary onClick={()=>this.handleUpload()}/>
+                            </div>}
+                        </CardText>
+                    </Card>
+                </Row>
+                <Row>
+                    <Card raised>
+                        <CardTitle title="Existing Images" avatar={<IconButton icon="refresh" onClick={()=>this.props.getImages()}/>}/>
+                        <CardText>
+                            <List selectable ripple>
+                                {this.mapImages()}
+                            </List>
+                        </CardText>
+                    </Card>    
+                </Row>
             </Col>
-            <Col md={6}>
-                <Card raised style={{marginBottom: "16px"}}>
-                    <CardTitle title="Currently Selected Image"/>
-                    <CardText>
-                        {selectedImage !== false && 
-                        <Row>
-                            <Col md={8}>
-                                {selectedImage && <img 
-                                    src={selectedImage.data}
-                                    style={{width: "100%", height: "auto"}}
-                                />}
-                            </Col>
-                            <Col md={4}>
-                                {selectedImage && <div><h4>
-                                    {selectedImage.name}
-                                </h4>
-                                <Button label="deselect image" accent raised onClick={()=>this.setState({selectedImage: false})} /><br /><br />
-                                <Button label="display on viewscreen" primary raised onClick={()=>this.props.setImage(selectedImage.id)} />
-                                </div>}
-                            </Col>
-                        </Row>}
-                        {this.state.selectedImage === false && <p>No image has been selected</p>}
-                    </CardText>
-                </Card>
-            </Col>
-            <Col md={4}>
-                <Card raised>
-                    <CardTitle title="Upload New Images"/>
-                    <CardText>
-                        <BrowseButton icon="search" name="fileUpload" label="choose file" onChange={(e)=>this.handleFileSelect(e)}/>
-                        {this.state.fileData && <div>
-                            <img style={{width: "100%", height: "auto"}}src={this.state.fileData}/>
-                            <Input type="text" value={this.state.newImageName} onChange={val=>this.setState({newImageName: val})} label="Image name"/>
-                            <Button icon="file_upload" label="upload" raised primary onClick={()=>this.handleUpload()}/>
-                        </div>}
-                    </CardText>
-                </Card>
-            </Col>
-            <Col md={8}>
-                <Card raised>
-                    <CardTitle title="Existing Images" avatar={<IconButton icon="refresh" onClick={()=>this.props.getImages()}/>}/>
-                    <CardText>
-                        <List selectable ripple>
-                            {this.mapImages()}
-                        </List>
-                    </CardText>
-                </Card>
+            <Col md={5}> 
+                <Row>
+                    <Card raised style={{marginBottom: "16px"}}>
+                        <CardTitle title="Current Display Image"/>
+                        <CardText>
+                            {this.props.currentImage && 
+                            <Row>
+                                <Col md={8}>
+                                    {this.props.currentImage.id && <img 
+                                        src={this.props.currentImage.data}
+                                        style={{width: "100%", height: "auto"}}
+                                    />}
+                                </Col>
+                                <Col md={4}>
+                                    {this.props.currentImage.id && <div><p>
+                                        {this.props.currentImage.name}
+                                    </p>
+                                    <Button label="unset image" onClick={()=>this.props.setImage(false)}/>
+                                    </div>}
+                                </Col>
+                            </Row>}
+                            {!this.props.currentImage && <p>No image has been set.</p>}
+                        </CardText>
+                    </Card>
+                </Row>
+                <Row>
+                    <Card raised style={{marginBottom: "16px"}}>
+                        <CardTitle title="Currently Selected Image"/>
+                        <CardText>
+                            {selectedImage !== false && 
+                            <Row>
+                                <Col md={8}>
+                                    {selectedImage && <img 
+                                        src={selectedImage.data}
+                                        style={{width: "100%", height: "auto"}}
+                                    />}
+                                </Col>
+                                <Col md={4}>
+                                    {selectedImage && <div><h4>
+                                        {selectedImage.name}
+                                    </h4>
+                                    <Button label="deselect image" accent raised onClick={()=>this.setState({selectedImage: false})} /><br /><br />
+                                    <Button label="display on viewscreen" primary raised onClick={()=>this.props.setImage(selectedImage.id)} />
+                                    </div>}
+                                </Col>
+                            </Row>}
+                            {this.state.selectedImage === false && <p>No image has been selected</p>}
+                        </CardText>
+                    </Card>
+                </Row>
             </Col>
         </Row>
     }
